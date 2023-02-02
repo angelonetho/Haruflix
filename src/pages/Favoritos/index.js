82dc70604ca38c6b7df3d08563f377accee091ec
Angelo-Netho <angelonetho@gmail.com>
2023-02-02 00:10:41 -0300

    Manipulando lista de filmes

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -12,9 +12,20 @@ function Favoritos() {
         setFilmes(JSON.parse(minhaLista) || [])
     }, [])
 
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter( (item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes)
+        localStorage.setItem("@haruflix", JSON.stringify(filtroFilmes))
+    }
+
     return(
         <div className='meus-filmes'>
             <h1>Meus favoritos</h1>
+
+            {filmes.length === 0 && <span>Sua lista está vazia..</span>}
 
             <ul>
                 {filmes.map((item) => {
@@ -23,7 +34,7 @@ function Favoritos() {
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
